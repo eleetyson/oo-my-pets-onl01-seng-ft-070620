fe199ee48855ae396d354ad1fddd21c6d449377b
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-20 23:51:20 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,10 +2,13 @@ require 'pry'
 class Owner
   attr_reader :name, :species
   
-  
   def initialize(name)
     @name = name
     @species = "human"
+  end
+  
+  def say_species
+    
   end
   
 end
