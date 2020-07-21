79dd210104b0ece3292027a3670852d1e4df1f3a
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-21 00:09:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,12 +1,13 @@
 require 'pry'
 class Dog
   attr_reader :name
-  attr_accessor :owner
+  attr_accessor :owner, :mood
   
   
   def initialize(name, owner)
     @name = name
     @owner = owner
+    @mood = "nervous"
   end
   
 end
