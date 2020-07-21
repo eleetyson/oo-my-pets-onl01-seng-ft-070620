01924e0a3a5cd335c1ff7b4c5df6ceafc2925037
Ethan Lee-Tyson <eleetyson@gmail.com>
2020-07-21 00:21:24 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -25,6 +25,10 @@ class Owner
     Cat.all.select { |cat| cat.owner == self }
   end
   
+  def dogs
+    Dog.all.select { |dog| dog.owner == self }
+  end
+  
   def self.count
     self.all.length
   end
